3ab348c46894d2a1276bdef59c2669d308e82589
sugarpaser <155459194+sugarpaser@users.noreply.github.com>
2024-01-03 11:34:56 +0900

    4

--- js/board.js
+++ js/board.js
@@ -1,1 +1,12 @@
+/*
+totaPage : 총 게시글 수
+page_num : 한 페이지 당 출력되는 게시글 갯수
+block_num : 한번에 출력될 수 있는 최대 블록 수
+current_block : 현재 블록 위치
+data : 게시글 데이터를 담고 있는 객체 배열
+post_data_print(block) : 게시글 데이터 출력하기 / 매개변수 : 선택 블럭
+block_print(front_block) : 블럭 출력하기 / 매개변수 : 가장 앞에 오는 블럭
+*/
 
+// 총 게시글 수
+let totalPage = 1000;
